3df91117decdfdac78ae206cf2b0df84e760ef19
jonesconnor <connorj@hey.com>
2022-08-01 20:19:31 -0700

    Add function to get text from PDF and pass it to Polly

--- tts.py
+++ tts.py
@@ -1,6 +1,16 @@
 import boto3
+from PyPDF2  import PdfFileReader
 import constants as c
 
+# Function to get text from PDF
+def text_extractor(file):
+    with open(file, "rb") as f:
+        pdf = PdfFileReader(f)
+        page = pdf.getPage(0)
+        text = page.extractText()
+    return text
+
+# AWS Connection
 client = boto3.client(
     'polly',
     region_name = "us-west-2",
@@ -8,10 +18,15 @@ client = boto3.client(
     aws_secret_access_key = c.SECRET_ACCESS_KEY
 )
 
+# Grab text from sample document
+f = text_extractor("sample.pdf")
+
+# Pass contents to Polly
 response = client.synthesize_speech(VoiceId='Joanna',
     OutputFormat = 'mp3',
-    Text = 'Hi my name is Connor and I like air conditioning',
+    Text = f,
     Engine = 'neural')
 
+# Create mp3 file containing Polly's speech
 with open('speech.mp3', 'wb') as f:
     f.write(response['AudioStream'].read())
